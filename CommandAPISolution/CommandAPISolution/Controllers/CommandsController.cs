da8b91dc5ffd550fe8ed8720d0f38d5b99471606
avneeshverma1234 <avneesh_verma@yahoo.com>
2022-08-28 15:23:10 +0100

    Initial commit

---
+++ CommandAPISolution/CommandAPISolution/Controllers/CommandsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Dynamic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandAPISolution.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CommandsController: ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<string>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<IEnumerable<string>> Get()
+    {
+        return new string[] {"this", "is", "hard", "coded"};
+    }
+}
